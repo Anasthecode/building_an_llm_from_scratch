d3011d11a926a12525395a6311be6fce600df2da
Anas at McMaster <abdulaam@mcmaster.ca>
2026-05-25 22:48:04 -0400

    Directories and files for cleaner code

--- chapter 2/chapter2.py
+++ chapter 2/chapter2.py
@@ -2,6 +2,7 @@ from importlib.metadata import version
 import os
 import requests
 import re
+from urlReader import URLReader
 
 print("torch version:", version("torch"))
 print("tiktoken version:", version("tiktoken"))
@@ -10,41 +11,9 @@ print("tiktoken version:", version("tiktoken"))
 print("Tokenization: Chapter 2.2")
 
 cleanFile = "frankenstein_clean.txt"
+url = "https://www.gutenberg.org/files/84/84-0.txt"
 
-if not os.path.exists(cleanFile):
-    # Download and process the file
-    url = "https://www.gutenberg.org/files/84/84-0.txt"
-    print(f"Downloading from {url}...")
-    response = requests.get(url)
-    response.raise_for_status()
-    rawText = response.text
-
-    # Cleanup
-    startMarker = "\n\nLetter 1\n"
-    endMarker = "*** END OF THE PROJECT GUTENBERG EBOOK 84 ***"
-
-    if startMarker in rawText and endMarker in rawText:
-        novelText = rawText.split(startMarker)[1].split(endMarker)[0]
-        novelText = novelText.strip()
-
-        # Clean formatting BEFORE saving
-        novelText = novelText.replace('_', '') # Getting rid of italics
-        novelText = re.sub(r'\n\s*\n', '\n\n', novelText) # gettind rid of white space
-        # no need to git rid of bold fonts (*) as the end/start markers involve that already
-
-        with open(cleanFile, "w", encoding="utf-8") as f:
-            f.write(novelText)
-        print("Text saved to 'frankenstein_clean.txt'")
-        print(f"Total characters: {len(novelText)}")
-    else:
-        print("Could not find the start/end markers in the download file")
-        exit(1)
-else:
-    # File already exists scenario
-    print(f"Loading existing {cleanFile}...")
-    with open(cleanFile, "r", encoding="utf-8") as f:
-        novelText = f.read()
-    print(f"Loaded {len(novelText)} characters")
+novelText = URLReader.readFile(cleanFile, url)
 
 # I ended up using the same one as the book had, though it is slightly irrelevant as I did get rid of spaces in mine
 preprocessed = re.split(r'([,.:;?_!"()\']|--|\s)', novelText) 
---
+++ chapter 2/urlReader.py
@@ -0,0 +1,43 @@
+from importlib.metadata import version
+import os
+import requests
+import re
+
+class URLReader:
+  @staticmethod
+  def readFile(cleanFile, url):
+    if not os.path.exists(cleanFile):
+      # Download and process the file
+      print(f"Downloading from {url}...")
+      response = requests.get(url)
+      response.raise_for_status()
+      rawText = response.text
+
+      # Cleanup
+      startMarker = "\n\nLetter 1\n"
+      endMarker = "*** END OF THE PROJECT GUTENBERG EBOOK 84 ***"
+
+      if startMarker in rawText and endMarker in rawText:
+        novelText = rawText.split(startMarker)[1].split(endMarker)[0]
+        novelText = novelText.strip()
+
+        # Clean formatting BEFORE saving
+        novelText = novelText.replace('_', '') # Getting rid of italics
+        novelText = re.sub(r'\n\s*\n', '\n\n', novelText) # gettind rid of white space
+        # no need to git rid of bold fonts (*) as the end/start markers involve that already
+
+        with open(cleanFile, "w", encoding="utf-8") as f:
+            f.write(novelText)
+        print("Text saved to 'frankenstein_clean.txt'")
+        print(f"Total characters: {len(novelText)}")
+      else:
+          print("Could not find the start/end markers in the download file")
+          exit(1)
+    else:
+      # File already exists scenario
+      print(f"Loading existing {cleanFile}...")
+      with open(cleanFile, "r", encoding="utf-8") as f:
+        novelText = f.read()
+      print(f"Loaded {len(novelText)} characters")
+
+    return novelText
